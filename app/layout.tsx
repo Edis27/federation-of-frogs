ae00510d3efd83865fcecc5eb5431b19b351e638
Edward Payne <epayne2023@edwards-air.mynetworksettings.com>
2025-12-19 15:37:10 -0500

    web appearance changes and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,10 @@ const WalletConnectProvider = ({ children }: PropsWithChildren<{}>) => (
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
        <html lang="en">
+            <head>
+               <title>Federation of Frogs</title>
+               <meta name="description" content="Join the Federation and generate your frog today!" />
+           </head>
            <body>
                <WalletConnectProvider>
                    {children}
