876c05b044302500ab6d41416aeed3065cd94f5e
Edward Payne <epayne2023@edwards-air.mynetworksettings.com>
2025-12-20 11:04:02 -0500

    changed mint price to 1000 coins

--- app/mint/page.tsx
+++ app/mint/page.tsx
@@ -25,7 +25,7 @@ import {
 
 const RIBBIT_MINT_ADDRESS = new PublicKey("8aW5vwBWQP3vTqqHGSGs6MSqnRkSvHnti96b55ygpump");
 const TREASURY_WALLET = new PublicKey("6hNozPrcywMv5Lyx6VuqaSooWCsNsvQyoXie9W4u8RTK");
-const MINT_COST_AMOUNT = 100;
+const MINT_COST_AMOUNT = 1000;
 const RIBBIT_TOKEN_DECIMALS = 6;
 
 
